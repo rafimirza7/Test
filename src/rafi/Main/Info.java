e9ff07fff79e540397785fe7898a4b6c39223ffe
rafi <rafimirza775@gmail.com>
2021-12-28 07:56:05 -0800

    initial commit

---
+++ src/rafi/Main/Info.java
@@ -0,0 +1,4 @@
+package rafi.Main;
+
+public class Info {
+}
---
+++ src/rafi/Main/Main.java
@@ -0,0 +1,8 @@
+package rafi.Main;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
